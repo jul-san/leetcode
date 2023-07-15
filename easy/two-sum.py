0579c5681567e7c0ac06e72ad5b3552384c08ad1
Julian Sanchez <108894992+jul-san@users.noreply.github.com>
2023-07-15 12:08:21 -0400

    added shout-out

--- easy/two-sum.py
+++ easy/two-sum.py
@@ -1,6 +1,8 @@
 '''
     July 14, 2023
 
+    **Shout-out to John Huber from Northrop for teaching me about dictionaries 🤜🤛**
+
     Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
     You may assume that each input would have exactly one solution, and you may not use the same element twice.
     You can return the answer in any order.
